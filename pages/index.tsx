3e006ba79ad62bc8b8c3d310c07752651bf13d3e
Kazuto Kusama <jacopen@gmail.com>
2022-07-17 14:23:28 +0900

    fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,4 @@
 import type { NextPage } from 'next'
-import Box, { BoxProps } from '@mui/material/Box'
-import { Player } from '../src/components/player'
-import { Clock } from '../src/components/clock'
-import { css } from '@mui/styled-engine'
-import { data } from '../src/data'
-
 
 const Home: NextPage = () => {
   return (
